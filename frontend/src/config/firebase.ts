423973360128e181be266447faa2b40f2e595b8b
Bannuru Veerendra <88670654+mr-veeru@users.noreply.github.com>
2025-03-26 12:11:33 +0530

    Update firebase.ts

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAcDPeZJuya0i5fz-bG6B9ot_cx2d4y8JM",
-  authDomain: "devshare-68.firebaseapp.com",
-  projectId: "devshare-68",
-  storageBucket: "devshare-68.firebasestorage.app",
-  messagingSenderId: "107679324180",
-  appId: "1:107679324180:web:ac7fbc0ab7c5525e9aef01",
-  measurementId: "G-GVXF8PQ6D0"
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR-AUTH-DOMAIN",
+  projectId: "YOUR-PROJECT-ID",
+  storageBucket: "YOUR-STORAGE-BUCKET",
+  messagingSenderId: "YOUR-MESSAGING-SENDER-ID",
+  appId: "YOUR-APP-ID",
+  measurementId: "YOUR-MEASUREMENT-ID"
 };
 
 // Initialize Firebase
